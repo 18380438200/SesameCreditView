1daff8e0cc4e46db3ffbbfeb781c3fdcf1e32f4f
apple <18201497620@163.com>
2020-07-27 14:14:14 +0800

    initCommit

---
+++ app/src/main/java/com/example/sesamecreditview/SesameCreditView.java
@@ -0,0 +1,24 @@
+package com.example.sesamecreditview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * create by libo
+ * create on 2020/7/27
+ * description 支付宝芝麻信用自定义view
+ */
+public class SesameCreditView extends View {
+
+    public SesameCreditView(Context context) {
+        super(context);
+    }
+
+    public SesameCreditView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
